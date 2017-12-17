5a0049feafa756df4198c904569a69408b8e7e3a
Daniel Dylewsky <dylewsky@uw.edu>
2017-12-17 15:53:01 -0800

    Analysis of ExtDMD with features = polynomials up to 3rd order

---
+++ Tao Example/example2_extDMD_analytics.m
@@ -0,0 +1,32 @@
+load('mr_res_2_ext.mat');
+
+qBest = 5;
+nSplit = 128;
+r = length(mr_res{qBest,1}.Omega);
+nFeat = size(mr_res{qBest,q}.x,1);
+
+allModes = zeros(nFeat,r,nSplit);
+
+for k = 1:nSplit
+    try
+        omega = mr_res{qBest,k}.Omega;
+    catch ME
+        disp(['Rank Deficient at k = ' num2str(k)])
+        continue
+    end
+    om_sq = omega.*conj(omega);
+    [om_sq, sind] = sort(om_sq);
+    w = mr_res{qBest,k}.w;
+    if nnz(w == Inf) ~= 0
+        disp('Infinite-Valued Mode');
+        continue
+    elseif nnz(isnan(w)) ~= 0
+        disp('NaN in Mode');
+        continue
+    end
+    w = w(:,sind);
+    allModes(:,:,k) = w;
+end
+
+absModes = abs(allModes);
+meanModes = nansum(absModes,3);
