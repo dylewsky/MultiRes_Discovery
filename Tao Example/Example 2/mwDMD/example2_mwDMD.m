a7cceb7a1c09bce869936a6546ab7181604870a8
ded114 <dandylewsky@gmail.com>
2018-01-25 22:20:10 -0800

    Add more diagnostic plots to mwDMD results; fix some errors in implementation of DMD (e.g. changing each window's time vector to start at 0 + shifting back for reconstruction)

--- Tao Example/Example 2/mwDMD/example2_mwDMD.m
+++ Tao Example/Example 2/mwDMD/example2_mwDMD.m
@@ -12,13 +12,17 @@ imode = 1; %parameter for optdmd code
 %      or columns of varargin{2} (should be at least r
 %      columns in varargin{2})
 
+nComponents = 2;
+use_last_freq = 1;
+
 wSteps = 11000;
 nSplit = 20; %number of windows if they were non-overlapping
 nSteps = wSteps * nSplit / 2;
 nVars = size(x,1);
 thresh_pct = 1;
 
-stepSize = wSteps/10;
+% stepSize = wSteps/10;
+stepSize = 100;
 
 nSlide = floor((nSteps-wSteps)/stepSize);
 
@@ -38,10 +42,22 @@ for k = 1:nSlide
     mr_res{k}.x = xSample;
     xSample = xSample.*repmat(lv_kern,nVars,1); %round off corners
     mr_res{k}.t = tSample;
-    [w, e, b] = optdmd(xSample,tSample,r,imode);
+    
+    c = mean(xSample,2);
+    xSample = xSample - repmat(c,1,size(xSample,2));
+    t_start = tSample(1);
+    tSample = tSample - t_start;
+    if (exist('e_init','var')) && (use_last_freq == 1)
+        [w, e, b] = optdmd(xSample,tSample,r,imode,[],e_init);
+    else
+        [w, e, b] = optdmd(xSample,tSample,r,imode);
+    end
+    e_init = e;
     mr_res{k}.w = w;
     mr_res{k}.Omega = e;
     mr_res{k}.b = b;
+    mr_res{k}.c = c;
+    mr_res{k}.t_start = t_start;
 end
 
 
@@ -51,7 +67,6 @@ if exist('mr_res','var') == 0
     load('mwDMD_mr_res.mat');
 end
 
-nComponents = 2;
 % nBins = 64;
 
 
@@ -125,16 +140,6 @@ figure('units','pixels','Position',[100 100 1200 400])
 %     nSplit = 2^(j-1);
 
 %     om_spec = zeros(nVars,nSteps);
-subplot(2,2,4);
-plot(t_PoT,real(x_PoT)) %plot ground truth
-xlabel('t')
-ylabel('Measurement Data')
-
-xMax = max(max(abs(x_PoT)));
-
-ylim(1.5*[-xMax, xMax]);
-hold on
-
 all_om = [];
 
 
@@ -158,16 +163,21 @@ yl = ylim;
 for c = 1:nComponents
     plot([km_centroids(c), km_centroids(c)], yl,'Color',colorList{c},'LineWidth',2)
 end
-
+xr = zeros(size(x(:,1:nSteps))); %reconstructed time series
+xn = zeros(nSteps,1); %count # of windows contributing to each step
 for k = 1:nSlide
     w = mr_res{k}.w;
     b = mr_res{k}.b;
     Omega = mr_res{k}.Omega;
     om_class = mr_res{k}.om_class;
     t = mr_res{k}.t;
+    c = mr_res{k}.c;
+    t_start = mr_res{k}.t_start;
     tShift = t-t(1); %compute each segment of xr starting at "t = 0"
 %     t_nudge = 5;
-
+    xr_window = w*diag(b)*exp(Omega*(t-t_start)) + c;
+    xr(:,(k-1)*stepSize+1:(k-1)*stepSize+wSteps) = xr(:,(k-1)*stepSize+1:(k-1)*stepSize+wSteps) + xr_window;
+    xn((k-1)*stepSize+1:(k-1)*stepSize+wSteps) = xn((k-1)*stepSize+1:(k-1)*stepSize+wSteps) + 1;
 
     % Plot |omega|^2 spectrum
     om_sq = conj(Omega).*Omega;
@@ -204,8 +214,20 @@ for k = 1:nSlide
         hold on
     end
     title('|\omega|^2 Spectra (Moving Window)');
-
 end
+
+subplot(2,2,4);
+plot(t_PoT,real(x_PoT),'k-') %plot ground truth
+xlabel('t')
+ylabel('Measurement Data')
+xMax = max(max(abs(x_PoT)));
+ylim(1.5*[-xMax, xMax]);
+hold on
+
+xr = xr./repmat(xn.',4,1); %weight xr so all steps are on equal footing
+plot(t_PoT,real(xr),'b-') %plot averaged reconstruction
+title('Input (Black); DMD Recon. (Blue)');
+
 
 if export_result == 1
     if lv == 1
@@ -224,30 +246,45 @@ if exist('mr_res','var') == 0
 end
 
 allModes = zeros(nSlide,r,r);
-catList = perms(1:r);
+allFreqs = zeros(nSlide,r);
+catList = flipud(perms(1:r));
 modeCats = zeros(nSlide,r);
 modeCats(1,:) = 1:r; %label using order of 1st modes
 allModes(1,:,:) = mr_res{1}.w;
+allFreqs(1,:) = mr_res{1}.Omega;
 for k = 2:nSlide
     w_old = mr_res{k-1}.w;
+    w = mr_res{k}.w;
     permDists = zeros(size(catList,1),1);
-    for perm = 1:size(catList,1)
-        permDists(perm) = norm(w(:,catList(perm,:))-w_old(:,catList(perm,:)));
+    for np = 1:size(catList,1)
+        permDists(np) = norm(w(:,catList(np,:))-w_old);
     end
     [~,minCat] = min(permDists);
     modeCats(k,:) = catList(minCat,:);
     allModes(k,:,:) = w(:,catList(minCat,:));
+    allFreqs(k,:) = mr_res{k}.Omega(catList(minCat,:));
 end
+meanFreqsSq = sum(allFreqs.*conj(allFreqs),1)/nSlide;
 
-figure
+figure('units','pixels','Position',[100 100 1366 768])
 colorlist = {'k','r','b','g'};
 w = mr_res{k}.w;
 wPlots = cell(r,r);
 wTrails = cell(r,r);
 trailLength = 10; %in steps
+% Plot time series
+subplot(4,4,5:16)
+plot(TimeSpan(1:nSteps),x(:,1:nSteps),'LineWidth',1.5)
+xlim([TimeSpan(1) TimeSpan(nSteps)])
+hold on
+lBound = plot([mr_res{1}.t(1) mr_res{1}.t(1)],ylim,'r-','LineWidth',2);
+hold on
+rBound = plot([mr_res{1}.t(end) mr_res{1}.t(end)],ylim,'r-','LineWidth',2);
+hold on
+
 % Plot 1st frame
 for dim = 1:r
-    subplot(2,2,dim)
+    subplot(4,4,dim)
     wi = w(dim,:);
     for j = 1:r
         wPlots{dim,j} = plot(real(wi(j)),imag(wi(j)),'o','Color',colorlist{j},'MarkerSize',7);
@@ -255,16 +292,20 @@ for dim = 1:r
         wTrails{dim,j} = plot(real(wi(j)),imag(wi(j)),'-','Color',colorlist{j},'LineWidth',0.2);
         hold on
     end
-    hold off
     title(['Proj. Modes into Dimension ' num2str(dim)])
+    axis equal
     xlim([-1 1])
     ylim([-1 1])
+    plot(xlim,[0 0],'k:')
+    hold on
+    plot([0 0],ylim,'k:')
+    hold off
 end
 %Plot subsequent frames
 for k = 2:nSlide
     w = mr_res{k}.w;
     for dim = 1:r
-        subplot(2,2,dim)
+%         subplot(4,4,dim)
         wi = w(dim,:);
         for j = 1:r
             wPlots{dim,j}.XData = real(wi(j));
@@ -278,6 +319,43 @@ for k = 2:nSlide
             end
         end
     end
-    pause(0.2)
+    lBound.XData = [mr_res{k}.t(1) mr_res{k}.t(1)];
+    rBound.XData = [mr_res{k}.t(end) mr_res{k}.t(end)];
+    pause(0.05)
 end
+
+%% Times series of mode coordinates
+figure('units','pixels','Position',[100 100 1366 768])
+for j = 1:r
+    subplot(r,2,2*j-1)
+    plot(real(allModes(:,:,j)))
+    title(['Re[w_' num2str(j) ']'])
+    legend('a','b','r','\theta')
+    if j == r
+        xlabel('Window #')
+    end
+    subplot(r,2,2*j)
+    plot(imag(allModes(:,:,j)))
+    title(['Im[w_' num2str(j) ']'])
+    legend('a','b','r','\theta')
+    if j == r
+        xlabel('Window #')
+    end
+end
+
+%% Show how modes are reflections of one another
+figure('units','pixels','Position',[100 100 1366 768])
+subplot(2,2,1)
+plot(real(allModes(:,:,1) - conj(allModes(:,:,2))))
+title('Re[w_1 - w_2^*]')
+subplot(2,2,2)
+plot(imag(allModes(:,:,1) - conj(allModes(:,:,2))))
+title('Im[w_1 - w_2^*]')
+
+subplot(2,2,3)
+plot(real(allModes(:,:,3) - conj(allModes(:,:,4))))
+title('Re[w_3 - w_4^*]')
+subplot(2,2,4)
+plot(imag(allModes(:,:,3) - conj(allModes(:,:,4))))
+title('Im[w_3 - w_4^*]')
 
